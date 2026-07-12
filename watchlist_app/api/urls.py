e7a8fad2321761e5fa1158f35e36a14f692a51c1
nahidul arafat <nahidularaf@gmail.com>
2026-07-12 23:10:14 +0600

    data entry

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,7 +2,7 @@
 from django.urls import  path
 
 from watchlist_app import views
-
+from watchlist_app.api import views
 urlpatterns = [
     
     path('/',views.movie_list,name='movie-list'),
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,9 +3,22 @@ from . import serializers
 from rest_framework.response import Response   
 from rest_framework.decorators import api_view
 
-def movie_list(movielist):
+# @api_view(['GET'])
+# def movie_list(request):
+#     movies = models.Movielist.objects.all()
+#     serializer = serializers.MovieSerializer(movies, many=True)
+#     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+   if request.method == 'GET':   
     movies = models.Movielist.objects.all()
     serializer = serializers.MovieSerializer(movies, many=True)
     return Response(serializer.data)
-
-
+   elif request.method == 'POST':
+     serializer = serializers.MovieSerializer(data=request.data)
+     if serializer.is_valid():
+       serializer.save()
+       return Response(serializer.data, status=201)
+     return Response(serializer.errors, status=400)   
